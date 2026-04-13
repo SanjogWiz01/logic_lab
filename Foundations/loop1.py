f57b549b7e037497125175324bd38cc8073453f3
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-04-13 19:16:35 +0545

    added looping

--- Foundations/loop1.py
+++ Foundations/loop1.py
@@ -1,3 +1,14 @@
-num = ['1','2','3']
-for num in num:
-    print(f"i am eating {num}")
+'''#
+# num = ['1','2','3']
+ #for num in num:
+ #   print(f"i am eating {num}") '''
+def funct(n):
+    if (n==0 or n==1):
+     return 1
+    else:
+       return n * funct(n-1)
+
+ 
+fact=int(input(" kati ko fact chaiyoo"))
+answer=funct(fact)
+print(answer)
