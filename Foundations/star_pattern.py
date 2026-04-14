9837224522b21a9f5b43baaabaa7cabf01c011c9
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-04-14 12:46:38 +0545

    ADDED STAR PATTERNS

--- Foundations/star_pattern.py
+++ Foundations/star_pattern.py
@@ -1,2 +1,8 @@
-def star(n,k):
- if ( k == n):
+n=int(input("Kti ota tsar cahiyoo tali van"))
+print("Normal patern")
+for i in range (1 ,n+1):
+    print(i * "*")
+print("Vertical star")  
+n=int(input(" ulto kati ota ko"))
+for i in range(n , 0):
+    print(i * "*")
